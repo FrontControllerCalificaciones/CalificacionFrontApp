a450595bf431243261fe78da84ca0c2c8a8346b2
Hp <Hp@DESKTOP-FGQ1DTU>
2018-11-16 13:29:06 -0600

    ooo

--- AppCalificacionesGit/src/mx/edu/uttt/operaciones/operaciones.java
+++ AppCalificacionesGit/src/mx/edu/uttt/operaciones/operaciones.java
@@ -3,14 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+/*
 package mx.edu.uttt.operaciones;
+
+import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
+import vista_profe.espacio_profe;
+import run.main;
 
 /**
  * 
  * @author Sammy Guergachi <sguergachi at gmail.com>
- */
+ *//**
 public class operaciones {
-String popo="creacion clase";
-String laksal="creacion clase";
+
+    //se crean las vistas como variables 
+    private espacio_profe profe;
+   private main login;
+   //se asigna a cada variable su vista
+   
+   
+   
+   public Operaciones(){
+      login= new main();
+      profe= new espacio_profe();
+   }
+//se extrae lo que ungresas en el main o login 
+   public void ingrsarusuario(String usuario){
+      //si usuaario es igual a maestro entonces te manda a la vista del profesor 
+      //donde podras promediar a el alumno
+       if(usuario.equalsIgnoreCase("luis")){
+         profe.show();
+           System.out.println("validacion exitosa");
+      }
+      else if  (usuario.equalsIgnoreCase("jesus")) {
+          profe.show();
+           System.out.println("validacion de jesus exitosa");
+       }
+      //si no te regresa denuevo a el login
+      else{
+         System.out.println("fallo validacion");
+          JOptionPane.showMessageDialog(null, "!!!!no existe este profesor registrado"+"\n"+"ingresar de profesor existente!!!!");
+      }	
+   }public void llll(){
+   login.show();
+   } 
+    
 }
+ */
