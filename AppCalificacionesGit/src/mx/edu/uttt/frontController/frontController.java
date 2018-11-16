ae73b7b20a5fa970534600c089eb10a39d49cb1f
Alumno <Alumno@TL1PC24>
2018-11-16 13:25:06 -0600

    codificacion de la clase front controller

--- AppCalificacionesGit/src/mx/edu/uttt/frontController/frontController.java
+++ AppCalificacionesGit/src/mx/edu/uttt/frontController/frontController.java
@@ -10,7 +10,37 @@ package mx.edu.uttt.frontController;
  * 
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
+/*
 public class frontController {
 
-String popo="creacion clase";
+ private Operaciones operacion;
+    //se instancia para meter operaciones en una variable
+    public FrontController(){
+      operacion = new  Operaciones();
+     
+    } //solo es un metodo que va a retornar un booleano
+private boolean estadodevalidacion(){
+        
+
+        System.out.println("validando autentificacion");
+      return true;
+      
+   }
+//con el parametro string sabremos que tipo de usuARIO es ya que este sera develto desde 
+//el login segun lo que ingrese en el jtextfield
+   private void tipodeusuario(String usuario){
+      System.out.println("el usuario es : " + usuario);
+   }
+   //recibe el mism parametro que es lo que ingresaras en el jtextfield
+    public void realizandovalidacion(String usuario){
+      //solo se pone que tipo de uusuario sera
+      tipodeusuario(usuario);
+   //aqui se crea la condicion para que estadodevalidacion se true y lo sera
+   //si cumple con las codiciones del  metodo ingresarusuario que esta en la clase Operaciones ya que 
+   //la variable operacion es una instancia de Operaciones
+      if(estadodevalidacion()){
+         operacion.ingrsarusuario(usuario);
+      }	
+   }
 }
+*/
